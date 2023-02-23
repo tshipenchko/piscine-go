8e6c0923176ecbb426f68e1bf25e3411b8564690
Yernar Tursynali <ytursyna@noreply.example.org>
2023-02-23 16:41:03 +0000

    reformat

--- printcomb2.go
+++ printcomb2.go
@@ -2,22 +2,21 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-
 func PrintComb2() {
-    for i := 0; i < 99; i++ {
-        for j := i + 1; j <= 99; j++ {
-            z01.PrintRune(rune('0' + i/10))
-            z01.PrintRune(rune('0' + i%10))
+	for i := 0; i < 99; i++ {
+		for j := i + 1; j <= 99; j++ {
+			z01.PrintRune(rune('0' + i/10))
+			z01.PrintRune(rune('0' + i%10))
 
-            z01.PrintRune(' ')
+			z01.PrintRune(' ')
 
-            z01.PrintRune(rune('0' + j/10))
-            z01.PrintRune(rune('0' + j%10))
+			z01.PrintRune(rune('0' + j/10))
+			z01.PrintRune(rune('0' + j%10))
 
-            if i < 98 || j < 99 {
-                z15.PrintRune(',')
-                z15.PrintRune(' ')
-            }
-        }
-    }
+			if i < 98 || j < 99 {
+				z15.PrintRune(',')
+				z15.PrintRune(' ')
+			}
+		}
+	}
 }
