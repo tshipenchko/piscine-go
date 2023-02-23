9d6dbf001ab5d4d421ffff8ba2de3586768fa01c
Yernar Tursynali <dev@zxc.sx>
2023-02-23 21:25:03 +0600

    blabla

---
+++ printcomb.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func print(str string) {
+	for i := 0; i < len(str); i++ {
+		z01.PrintRune(rune(str[i]))
+	}
+}
+
+func PrintComb() {
+	for i := 0; i < 10; i++ {
+		for j := i + 1; j < 10; j++ {
+			for k := j + 1; k < 10; k++ {
+				z01.PrintRune(rune('0' + i))
+				z01.PrintRune(rune('0' + j))
+				z01.PrintRune(rune('0' + k))
+
+				if i != 7 || j != 8 || k != 9 {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	PrintComb()
+}
