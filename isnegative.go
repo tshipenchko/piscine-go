1853c8c89f352463604d4035bf298978bfc4380d
Yernar Tursynali <dev@zxc.sx>
2023-02-23 18:38:03 +0600

    m

--- isnegative.go
+++ isnegative.go
@@ -5,9 +5,9 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	result := 'T'
 
-	if nb < 0 {
+	if nb > 0 {
 		result = 'F'
 	}
 
-	z01.PrintRune(result)
+	z01.PrintRune(result + '\n')
 }
