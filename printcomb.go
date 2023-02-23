772316e53dbf9dda61aff63049286cfb0ae2421f
Yernar Tursynali <dev@zxc.sx>
2023-02-23 21:25:50 +0600

    ggg

--- printcomb.go
+++ printcomb.go
@@ -2,12 +2,6 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func print(str string) {
-	for i := 0; i < len(str); i++ {
-		z01.PrintRune(rune(str[i]))
-	}
-}
-
 func PrintComb() {
 	for i := 0; i < 10; i++ {
 		for j := i + 1; j < 10; j++ {
