2b48a67415fc3500a6bae0bde26b39fdc69a75db
Yernar Tursynali <dev@zxc.sx>
2023-02-23 21:38:10 +0600

    asas

--- printcomb2.go
+++ printcomb2.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := 0; i < 10; i++ {
-		for j := i + 1; j < 10; j++ {
+		for j := i; j < 10; j++ {
 			for k := 0; k < 10; k++ {
 				for l := k + 1; l < 10; l++ {
 					z01.PrintRune(rune('0' + i))
