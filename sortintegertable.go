e2d67b588cacce890cd3bffe03501b8134c49df9
Yernar Tursynali <dev@zxc.sx>
2023-02-24 17:18:26 +0600

    sds

--- sortintegertable.go
+++ sortintegertable.go
@@ -2,7 +2,12 @@ package piscine
 
 func SortIntegerTable(table []int) {
 	for i := 0; i < len(table)-1; i++ {
-		for j := i + 1; j < len(table); j++ {
+		for j := 0; j < len(table)-i-1; j++ {
+			if table[j] > table[j+1] {
+				temp := table[j]
+				table[j] = table[j+1]
+				table[j+1] = temp
+			}
 		}
 	}
 }
