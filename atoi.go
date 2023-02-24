66d2a452d8a2936b2ede205c13fff658e93fa02f
Yernar Tursynali <dev@zxc.sx>
2023-02-24 17:02:37 +0600

    sdsd

--- atoi.go
+++ atoi.go
@@ -1,43 +1,36 @@
 package piscine
 
 func Atoi(s string) int {
-	if s == "-2814715995976974226" {
-		return -2814715995976974226
-	}
-
 	result := 0
-
 	base := 1
-	for i := 1; i < len(s); i++ {
-		base *= 10
+
+	if len(s) == 0 {
+		return 0
 	}
 
-	sing := false
-	bim := 1
-	for i := 0; i < len(s); i++ {
-		if s[i] == '+' {
-			if sing {
-				return 0
-			}
-			bim = 1
-			sing = true
-			base /= 10
-		} else if s[i] == '-' {
-			if sing {
-				return 0
-			}
-			bim = -1
-			sing = true
-			base /= 10
-		} else if s[i] > '9' || s[i] < '0' {
+	if len(s) == 1 {
+		if s[0] > '9' || s[0] < '0' {
 			return 0
-		} else {
-			num := int(s[i]) - '0'
-
-			result += num * base
-			base /= 10
 		}
+		return int(s[0] - '0')
 	}
 
-	return result * bim
+	for i := len(s) - 1; i > 1; i-- {
+		if s[i] > '9' || s[i] < '0' {
+			return 0
+		}
+
+		num := int(s[i] - '0')
+		result += num * base
+		base *= -10
+	}
+
+	if s[0] == '-' {
+		result *= -1
+	}
+	if s[0] > '9' || s[0] < '0' || s[0] != '+' {
+		return 0
+	}
+
+	return result
 }
