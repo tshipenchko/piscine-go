47bcf0a2b13685cbbb00e8b9ff5a1e5e4c68bf8f
Yernar Tursynali <dev@zxc.sx>
2023-02-27 19:20:16 +0600

    fix

--- recursivepower.go
+++ recursivepower.go
@@ -1,11 +1,18 @@
 package piscine
 
+func RecursivePowerIterator(result, nb, power int) int {
+	if power == 1 {
+		return result * nb
+	}
+	return RecursivePowerIterator(result*nb, nb, power-1)
+}
+
 func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if power == 1 {
-		return nb
+	if power == 0 {
+		return 1
 	}
-	return nb * RecursivePower(nb-1, power)
+	return RecursivePowerIterator(1, nb, power)
 }
