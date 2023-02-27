ec2ba68894759aafb1aa499dbb8b00194b8a9d95
Yernar Tursynali <dev@zxc.sx>
2023-02-27 20:26:02 +0600

    fix5

--- sqrt.go
+++ sqrt.go
@@ -1,14 +1,5 @@
 package piscine
 
-func SqrtIterator(nb, n int) int {
-	for i := 1; i < nb/2; i++ {
-		if i*i == nb {
-			return i
-		}
-	}
-	return 0
-}
-
 func Sqrt(nb int) int {
 	if nb == 0 {
 		return 0
@@ -16,5 +7,10 @@ func Sqrt(nb int) int {
 	if nb == 1 {
 		return 1
 	}
-	return SqrtIterator(nb, 1)
+	for i := 1; i <= nb/2; i++ {
+		if i*i == nb {
+			return i
+		}
+	}
+	return 0
 }
