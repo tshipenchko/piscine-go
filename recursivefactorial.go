f9c356a684210dbc25d32221092f1db960d0f958
Yernar Tursynali <dev@zxc.sx>
2023-02-27 18:24:36 +0600

    fix

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,8 +4,8 @@ func RecursiveFactorial(nb int) int {
 	if nb < 0 || nb > 30 {
 		return 0
 	}
-	if nb == 1 {
+	if nb == 0 {
 		return 1
 	}
-	return RecursiveFactorial(nb - 1)
+	return nb * RecursiveFactorial(nb-1)
 }
