42c57a2dbc1028c93568bae5f566e3f519bb3372
Yernar Tursynali <dev@zxc.sx>
2023-02-28 18:26:15 +0600

    fix

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, value := range s {
-		if !('A' <= value && value <= 'Z' || 'a' <= value && value <= 'z') {
+		if !('A' <= value && value <= 'Z' || 'a' <= value && value <= 'z' || '0' <= value && value <= '9') {
 			return false
 		}
 	}
