f06e2b5cd5ac2d23b8a60e70d5ff09a8660a946d
Yernar Tursynali <dev@zxc.sx>
2023-03-02 21:08:22 +0600

    done

---
+++ printnbrbase.go
@@ -0,0 +1,35 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrBase(nbr int, base string) {
+	result := ""
+	int_base := len(base)
+
+	for i := 0; i < int_base; i++ {
+		for j := i + 1; j < int_base; j++ {
+			if base[i] == base[j] {
+				z01.PrintRune('N')
+				z01.PrintRune('V')
+				return
+			}
+		}
+	}
+
+	sign := ""
+	if nbr < 0 {
+		sign = "-"
+		nbr *= -1
+	}
+
+	for nbr > 0 {
+		result = string(base[nbr%int_base]) + result
+		nbr /= int_base
+	}
+
+	result = sign + result
+
+	for _, char := range result {
+		z01.PrintRune(rune(char))
+	}
+}
