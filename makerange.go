8100da0ab53fbad7cbe03be9804789e61c1d2e0c
Yernar Tursynali <dev@zxc.sx>
2023-03-03 17:13:08 +0600

    din don

---
+++ makerange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	var out []int
+
+	if min > max {
+		return out
+	}
+
+	out = make([]int, max-min)
+	for i := min; i < max; i++ {
+		out[i-min] = i
+	}
+}
