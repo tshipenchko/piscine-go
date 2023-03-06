a8ee8543c8ec01692a82046fd48a91b31399fe93
Yernar Tursynali <dev@zxc.sx>
2023-03-06 18:45:02 +0600

    din don

---
+++ displayfile/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	filename := os.Args[1]
+
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s", content)
+}
