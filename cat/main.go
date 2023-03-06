de2e6cec6c528f8e5d1fda1524e18d5507b8578e
Yernar Tursynali <dev@zxc.sx>
2023-03-06 19:20:46 +0600

    Done

---
+++ cat/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) != 0 {
+		for _, filename := range args {
+			file, err := os.Open(filename)
+			if err != nil {
+				PrintString("ERROR: open ", filename, ": No such file or directory\n")
+				os.Exit(1)
+			}
+			io.Copy(os.Stdout, file)
+		}
+	} else {
+		io.Copy(os.Stdout, os.Stdin)
+	}
+}
+
+func PrintString(strings ...string) {
+	for _, s := range strings {
+		for _, char := range s {
+			z01.PrintRune(char)
+		}
+	}
+}
