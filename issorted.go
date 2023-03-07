f1155a8f2000eca10d6e83c8cbf5c75b6fc89dc9
Yernar Tursynali <dev@zxc.sx>
2023-03-07 15:28:46 +0600

    Done

---
+++ issorted.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for j := 1; j < len(a); j++ {
+		i := j - 1
+		if f(a[i], a[j]) < 0 {
+			return false
+		}
+	}
+
+	return true
+}
