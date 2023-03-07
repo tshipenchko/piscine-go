08b724ecda276ae13227839e2559beeab14064df
Yernar Tursynali <dev@zxc.sx>
2023-03-07 15:38:42 +0600

    TryFix

--- issorted.go
+++ issorted.go
@@ -3,7 +3,7 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for j := 1; j < len(a); j++ {
 		i := j - 1
-		if f(a[i], a[j]) < 0 {
+		if f(a[i], a[j]) > 0 {
 			return false
 		}
 	}
