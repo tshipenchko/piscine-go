a9f28b64e557f69ab25964d6fdaa455110daf62a
Yernar Tursynali <dev@zxc.sx>
2023-03-07 15:21:05 +0600

    done

--- quests/quest09/countif.go
+++ quests/quest09/countif.go
@@ -11,12 +11,3 @@ func CountIf(f func(string) bool, tab []string) int {
 
 	return out
 }
-
-func IsNumeric(s string) bool {
-	for _, value := range s {
-		if value < '0' || value > '9' {
-			return false
-		}
-	}
-	return true
-}
