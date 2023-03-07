561b2747cc918b92ce0b221aef0b4d769eb70260
Yernar Tursynali <dev@zxc.sx>
2023-03-07 16:00:55 +0600

    Fix

--- issorted.go
+++ issorted.go
@@ -1,12 +1,24 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	ascending := true
+	descending := true
+
 	for j := 1; j < len(a); j++ {
 		i := j - 1
-		if f(a[i], a[j]) > 0 {
-			return false
+		if f(a[i], a[j]) < 0 {
+			ascending = false
+			break
 		}
 	}
 
-	return true
+	for j := 1; j < len(a); j++ {
+		i := j - 1
+		if f(a[i], a[j]) > 0 {
+			descending = false
+			break
+		}
+	}
+
+	return ascending || descending
 }
