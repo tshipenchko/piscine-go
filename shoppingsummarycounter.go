dd3ab560717a559ace0d7cd6408a945b8906a2d8
Yernar Tursynali <dev@zxc.sx>
2023-03-09 15:21:19 +0600

    Fix?

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -3,42 +3,22 @@ package piscine
 func ShoppingSummaryCounter(str string) map[string]int {
 	out := make(map[string]int)
 
-	for _, e := range split(str, " ") {
+	for _, e := range split(str) {
 		out[e]++
 	}
 
 	return out
 }
 
-func split(s, sep string) []string {
-	IsCurrentSep := func(i int) bool {
-		if len(s) >= len(sep)+i {
-			return s[i:i+len(sep)] == sep
-		}
-		return false
-	}
+func split(s string) []string {
+	out := []string{}
 
-	var out []string
-	var tmp []rune
 	j := 0
-
-	for i, char := range s {
-		if i < j {
-			continue
+	for i, e := range s {
+		if e == ' ' || i == len(s)-1 {
+			out = append(out, s[j:i])
+			j = i
 		}
-		if IsCurrentSep(i) {
-			j = i + len(sep)
-			if len(tmp) != 0 {
-				out = append(out, string(tmp))
-				tmp = tmp[:0]
-			}
-		} else {
-			tmp = append(tmp, char)
-		}
-	}
-
-	if len(tmp) != 0 {
-		out = append(out, string(tmp))
 	}
 
 	return out
