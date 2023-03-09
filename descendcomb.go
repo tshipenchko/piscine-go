772077d3ab83c79a7772e1cf8bd03e4b7fe9c8fc
Yernar Tursynali <dev@zxc.sx>
2023-03-09 13:05:31 +0600

    partly

--- descendcomb.go
+++ descendcomb.go
@@ -1,4 +1,12 @@
 package piscine
 
 func DescendComb() {
+	for a := 9; a >= 0; a-- {
+		for b := 9; a >= 1; b-- {
+			for c := 9; c >= 0; c-- {
+				for d := 8; d >= 1; d-- {
+				}
+			}
+		}
+	}
 }
