056b3dcc018619d0ece52a1115edd99c4d4756e4
Yernar Tursynali <dev@zxc.sx>
2023-03-09 14:07:42 +0600

    Fix

--- abort.go
+++ abort.go
@@ -1,10 +1,10 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	return BubbleSort([5]int{a, b, c, d, e})[3]
+	return BubbleSort([]int{a, b, c, d, e})[3]
 }
 
-func BubbleSort(array [5]int) [5]int {
+func BubbleSort(array []int) []int {
 	for i := 0; i < len(array)-1; i++ {
 		for j := 0; j < len(array)-i-1; j++ {
 			if array[j] > array[j+1] {
