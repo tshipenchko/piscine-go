9b83fe7d97b1fe1db7b925e1ef19088370ba221e
Yernar Tursynali <dev@zxc.sx>
2023-03-14 19:43:27 +0600

    Done

--- listmerge.go
+++ listmerge.go
@@ -1,5 +1,10 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	l1.Tail.Next = l2.Head
+	if l1.Tail == nil {
+		l1.Head = l2.Head
+		l1.Tail = l2.Tail
+	} else {
+		l1.Tail.Next = l2.Head
+	}
 }
