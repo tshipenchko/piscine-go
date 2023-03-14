2b157c14e396cefcca50006a1ba18d1b14f51a80
Yernar Tursynali <dev@zxc.sx>
2023-03-14 17:23:02 +0600

    Done

---
+++ listforeach.go
@@ -0,0 +1,28 @@
+package piscine
+
+func ListForEach(l *List, f func(*NodeL)) {
+	curr := l.Head
+
+	for curr != nil {
+		f(curr)
+		curr = curr.Next
+	}
+}
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
