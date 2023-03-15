0ab23f3f05d09c844138e43db0e2bcaa782c0909
Yernar Tursynali <ytursyna@noreply.example.org>
2023-03-15 13:34:13 +0000

    Fix

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,20 +1,20 @@
 package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-    newN := &NodeI{Data: data_ref, Next: nil}
+	newN := &NodeI{Data: data_ref, Next: nil}
 
-    if l == nil || l.Data >= data_ref {
-        newN.Next = l
-        return newN
-    }
-    
-    curr := l
-    for curr.Next != nil && curr.Next.Data < data_ref {
-        curr = curr.Next
-    }
-    
-    newN.Next = curr.Next
-    curr.Next = newN
-    
-    return l
+	if l == nil || l.Data >= data_ref {
+		newN.Next = l
+		return newN
+	}
+
+	curr := l
+	for curr.Next != nil && curr.Next.Data < data_ref {
+		curr = curr.Next
+	}
+
+	newN.Next = curr.Next
+	curr.Next = newN
+
+	return l
 }
