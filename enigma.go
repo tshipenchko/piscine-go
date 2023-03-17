7ea8a8736170334d12e947e3899385567a56b831
Yernar Tursynali <dev@zxc.sx>
2023-03-17 14:27:42 +0600

    Fix

--- enigma.go
+++ enigma.go
@@ -1,5 +1,5 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	***a, *b, *******c, ****d = *******c, ***a, ****d, *b
+	***a, *******c, ****d, *b = *******c, ****d, *b, ***a
 }
