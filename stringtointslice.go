193a19c9720bcd6312cf58bfd94254587e83464f
Yernar Tursynali <dev@zxc.sx>
2023-03-17 14:14:28 +0600

    Fix

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,6 +1,9 @@
 package piscine
 
 func StringToIntSlice(str string) []int {
+	if len(str) == 0 {
+		return nil
+	}
 	out := make([]int, 0, len(str))
 	for _, c := range str {
 		out = append(out, int(c))
