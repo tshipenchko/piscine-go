f31e23d447f92827f2827baaccc61edebb3ead67
Yernar Tursynali <dev@zxc.sx>
2023-03-17 14:30:04 +0600

    Done

---
+++ shoppinglistsort.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ShoppingListSort(slice []string) []string {
+	for i := 0; i < len(slice)-1; i++ {
+		for j := 0; j < len(slice)-i-1; j++ {
+			if len(slice[j]) > len(slice[j+1]) {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+			}
+		}
+	}
+	return slice
+}
