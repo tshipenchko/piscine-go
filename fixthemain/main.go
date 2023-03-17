67efb4811de61490b8d1846fe99712dbf0c610f5
Yernar Tursynali <dev@zxc.sx>
2023-03-17 14:41:42 +0600

    Done

---
+++ fixthemain/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "github.com/01-edu/z01"
+
+const (
+	OPEN  = true
+	CLOSE = false
+)
+
+type Door struct {
+	State bool
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func OpenDoor(ptrDoor *Door) bool {
+	PrintStr("Door Opening...")
+	ptrDoor.State = OPEN
+	return true
+}
+
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...")
+	ptrDoor.State = CLOSE
+	return true
+}
+
+func IsDoorOpen(door Door) bool {
+	PrintStr("is the Door opened ?")
+	return door.State
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	return ptrDoor.State
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(*door) {
+		CloseDoor(door)
+	}
+	if IsDoorOpen(*door) {
+		CloseDoor(door)
+	}
+}
